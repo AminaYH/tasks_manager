44d9dc3434a579942becdb01381a113d5c8b4df0
mina218 <amina.yahia@eniso.u-sousse.tn>
2025-09-08 18:22:35 +0200

    fix: fix: make Turbo Streams broadcast for update and delete tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   after_create_commit -> { broadcast_prepend_to "tasks_list", partial: "tasks/task", locals: { task: self }, target: "tasks-list" }
-  after_update_commit -> { broadcast_replace_to self, partial: "tasks/task", locals: { task: self } }
-  after_destroy_commit -> { broadcast_remove_to self }
+  after_update_commit -> { broadcast_replace_to "tasks_list", partial: "tasks/task", locals: { task: self } }
+  after_destroy_commit -> { broadcast_remove_to "tasks_list" }
 end
